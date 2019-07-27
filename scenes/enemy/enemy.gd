e3643d996e9e1cd0a061284b708810096732fcf6
Wichamir <michciopl87@gmail.com>
2019-07-27 16:16:47 +0200

    Update1

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -1,16 +1,16 @@
-tool
 extends Area2D
 
 class_name Enemy
 
-export(Shape2D) var collision_shape setget setCollision
-export(Texture) var texture setget setTexture
+export(float) var hp
+export(float) var speed
+export(Vector2) var velocity
 
-func setCollision(new_collision):
-	collision_shape=new_collision
-	$CollisionShape2D.set_shape(collision_shape)
+func move():
+	pass
 
-func setTexture(new_texture):
-	texture=new_texture
-	$Sprite.set_texture(texture)
+func shoot():
+	pass
 
+func _process(delta):
+	move()
