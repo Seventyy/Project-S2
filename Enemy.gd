2ddbc34beb1073546a452f020347af7a371085ef
Wichamir <michciopl87@gmail.com>
2019-07-27 19:06:57 +0200

    Update2

---
+++ Enemy.gd
@@ -0,0 +1,4 @@
+extends Enemy
+
+func move(delta):
+	pass
--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -2,15 +2,35 @@ extends Area2D
 
 class_name Enemy
 
-export(float) var hp
-export(float) var speed
-export(Vector2) var velocity
+enum MovementType {target_list,target_list_looped}
 
-func move():
-	pass
+export(MovementType) var movement_type
+export(float) var hp
+export(float) var body_damage
+export(float) var speed
+
+export(Vector2) var target
+export(Array) var target_array=[Vector2(100,100),Vector2(500,200),Vector2(1000,600),Vector2(1200,50)]
+var target_array_iterator=0
+
+var velocity=Vector2()
+
+func move(delta):
+	if target!=position:
+		velocity=(target-position).normalized()*speed*delta
+		if velocity.length()>(target-position).length():
+			position=target
+		else:
+			position+=velocity
+	else:
+		if target_array_iterator<target_array.size()-1:
+			target_array_iterator+=1
+		else:
+			target_array_iterator=0
+		target=target_array[target_array_iterator]
 
 func shoot():
 	pass
 
 func _process(delta):
-	move()
+	move(delta)
